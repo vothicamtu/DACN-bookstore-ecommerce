8979179e1193a2da02075c30c74e4db413d4808c
AnhXuan04 <22130333@st.hcmuaf.edu.vn>
2026-05-28 01:36:04 +0700

    BE hiển thị toàn bộ sp từ db

---
+++ backend/src/main/java/cntt/dacn/backend/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package cntt.dacn.backend.repository;
+
+import cntt.dacn.backend.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+}
