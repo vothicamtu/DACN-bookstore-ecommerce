deb0b6621d4a2ae37fc5f06f552f0dce08ef9645
vothicamtu <22130308@st.hcmuaf.edu.vn>
2026-06-02 15:47:22 +0700

    fix FE BE cart, đặt hàng

---
+++ frontend/src/components/checkout/CheckoutSteps.tsx
@@ -0,0 +1,24 @@
+export default function CheckoutSteps() {
+  const steps = ["Giỏ hàng", "Đặt hàng", "Thanh toán", "Hoàn tất"];
+
+  return (
+    <div className="flex justify-center items-center gap-10">
+      {steps.map((step, index) => (
+        <div key={step} className="flex items-center gap-3">
+          <div
+            className={`
+            w-8 h-8 rounded-full
+            flex items-center justify-center text-sm
+
+            ${index === 1 ? "bg-[#A46A1F] text-white" : "bg-[#ECE7DA]"}
+            `}
+          >
+            {index + 1}
+          </div>
+
+          <span>{step}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
